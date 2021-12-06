c723d81a1f7366c417c0888e6159905c674ec382
yuranl <46729353+yuranl@users.noreply.github.com>
2021-12-05 21:42:12 -0500

    add text data

--- eval/wav_to_sgm.py
+++ eval/wav_to_sgm.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import speech_recognition as sr
+import codecs
 
 ###############################
 ####change below parameters####
@@ -11,8 +12,9 @@ mode = "ref" #reference speech
 #mode = "tst" #output speech to be evaluated
 setid = "1" #id used to identify this set of data
 
-input_path = "/output/"
-#input_path = "/reference/"
+#input_path = ""
+input_path = "output/"
+#input_path = "reference/"
 
 
 
@@ -21,22 +23,29 @@ input_path = "/output/"
 
 output_filename = mode + setid + ".sgm"
 input_filename_list = glob.glob(input_path + "*" + input_format)
+print("Files found at " + input_path + "*" + input_format + ":")
+print(input_filename_list)
 r = sr.Recognizer()
 
-with open(output_filename, 'w') as output:
+with codecs.open(output_filename, 'w', "utf-8") as output:
     output.write("<" + mode + "set setid=\"" + setid + "\" srclang =\"chs\" trglang = \"eng\">\n")
     output.write("<DOC docid=\"doc\" genre=\"text\" sysid=\"" + mode + setid + "\">\n")
     seg_id = 0
     for input_filename in input_filename_list:
-        with open(input_filename) as input:
+        with sr.AudioFile(input_filename) as input:
             seg_id += 1
-            print("Processing file: " + seg_id + "; File name: " + input_filename + "\n")
+            print("Processing file: " + str(seg_id) + "; File name: " + input_filename + "\n")
             audio = r.record(input)
-            try:
-                text = r.recognize_google(audio, language = 'en-US')
-                output.write("<seg id=" + seg_id + ">" + text + "</seg>\n")
-            except Exception as e:
-                print("Exception: "+str(e))
+            text = r.recognize_google(audio, language = 'zh-CN', show_all = True)
+            #print(text)
+            #print(text["alternative"][0]["transcript"])
+            output.write("<seg id=" + str(seg_id) + ">" + text["alternative"][0]["transcript"] + "</seg>\n")
+            #try:
+            #    text = r.recognize_google(audio, language = 'en-US')
+            #    print(text)
+            #except Exception as e:
+            #    print("Exception: "+str(e))
+        #output.write("<seg id=" + str(seg_id) + ">" + text + "</seg>\n")
     output.write("</DOC>\n")
     output.write("</" + mode + "set>")
 
