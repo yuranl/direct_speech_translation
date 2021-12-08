1710ce3a4d62c590f47ed0c21ab85651738dd08f
Andrewzhuyx <46278301+Andrewzhuyx@users.noreply.github.com>
2021-12-07 22:52:59 -0500

    buggy finetuning

---
+++ finetune.py
@@ -0,0 +1,118 @@
+from transformers import AutoModelWithLMHead, AutoTokenizer
+from transformers import pipeline
+from transformers import DataCollatorForSeq2Seq
+from torch.utils.data import DataLoader
+from translationData import translationDataset
+from transformers import AdamW
+from accelerate import Accelerator
+from transformers import get_scheduler
+from tqdm.auto import tqdm
+import torch
+
+
+translationTrain = translationDataset('./transcription_translation/')
+translationValidation = translationDataset('./translation_validation/')
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = AutoModelWithLMHead.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en", return_tensors="pt")
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+translation = pipeline("translation_chinese_to_english", model=model, tokenizer=tokenizer)
+text = "我不知道我在干嘛"
+translated_text = translation(text, max_length=40)[0]['translation_text']
+print(translated_text)
+train_dataloader = DataLoader(
+    translationTrain,
+    shuffle=True,
+    collate_fn=data_collator,
+    batch_size=16,
+)
+eval_dataloader = DataLoader(
+    translationValidation, collate_fn=data_collator, batch_size=8
+)
+optimizer = AdamW(model.parameters(), lr=2e-5, weight_decay = 0.001)
+accelerator = Accelerator()
+model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
+    model, optimizer, train_dataloader, eval_dataloader
+)
+
+
+num_train_epochs = 1
+num_update_steps_per_epoch = len(train_dataloader)
+num_training_steps = num_train_epochs * num_update_steps_per_epoch
+
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+
+
+
+def postprocess(predictions, labels):
+    predictions = predictions.cpu().numpy()
+    labels = labels.cpu().numpy()
+
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # Some simple post-processing
+    decoded_preds = [pred.strip() for pred in decoded_preds]
+    decoded_labels = [[label.strip()] for label in decoded_labels]
+    return decoded_preds, decoded_labels
+
+
+
+progress_bar = tqdm(range(num_training_steps))
+
+for epoch in range(num_train_epochs):
+    # Training
+    model.train()
+    for batch in train_dataloader:
+        outputs = model(**batch)
+        loss = outputs.loss
+        accelerator.backward(loss)
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+    # Evaluation
+    model.eval()
+    for batch in tqdm(eval_dataloader):
+        with torch.no_grad():
+            generated_tokens = accelerator.unwrap_model(model).generate(
+                batch["input_ids"],
+                attention_mask=batch["attention_mask"],
+                max_length=512,
+            )
+        labels = batch["labels"]
+
+        # Necessary to pad predictions and labels for being gathered
+        generated_tokens = accelerator.pad_across_processes(
+            generated_tokens, dim=1, pad_index=tokenizer.pad_token_id
+        )
+        labels = accelerator.pad_across_processes(labels, dim=1, pad_index=-100)
+
+        predictions_gathered = accelerator.gather(generated_tokens)
+        labels_gathered = accelerator.gather(labels)
+
+        decoded_preds, decoded_labels = postprocess(predictions_gathered, labels_gathered)
+        metric.add_batch(predictions=decoded_preds, references=decoded_labels)
+
+    results = metric.compute()
+    print(f"epoch {epoch}, BLEU score: {results['score']:.2f}")
+
+    # Save and upload
+    accelerator.wait_for_everyone()
+    unwrapped_model = accelerator.unwrap_model(model)
+    output_dir = "finetuned_model"
+    unwrapped_model.save_pretrained(output_dir, save_function=accelerator.save)
+    if accelerator.is_main_process:
+        tokenizer.save_pretrained(output_dir)
