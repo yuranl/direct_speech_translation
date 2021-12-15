2407b24a7afeb199a54d64c892623d20d3ce99a9
Uysal Koyuncu <zhuyz@seas.upenn.edu>
2021-12-15 16:57:25 -0500

    Baseline

--- evaluation-1214/simple_baseline_testset.py
+++ evaluation-1214/simple_baseline_testset.py
@@ -2,49 +2,63 @@ import speech_recognition as sr
 from googletrans import Translator
 from pydub import AudioSegment
 import torchaudio
-import os, glob, json, codecs, math
+import glob, json, codecs, os
+from google.cloud import translate_v2 as translate
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./stone-airfoil-335221-d8728d7be624.json"
+
+translate_client = translate.Client()
 r = sr.Recognizer()
 
-for name in glob.glob('./MT_data/test/audio/*.wav'):
-    json_name = './MT_data/test/' + name.split('\\')[1].split('.')[0] + '.json'
-    print(json_name)
+for name in glob.glob('./evaluation-1214/audio_train/*.wav'):
+    json_name = './evaluation-1214/' + name.split('\\')[1].split('.')[0] + '.json'
+    # print(json_name)
     data = []
     with codecs.open(json_name, 'r', "utf-8") as f:
         for line in f:
             data.append(json.loads(line))
     # print(data)
 
-
     transcriptions = [rec["transcript"] for rec in data]
     offsets = [rec["offset"] for rec in data]
     durations = [rec["duration"] for rec in data]
 
-    speech_array, sampling_rate = torchaudio.load(name)
-    resampler = torchaudio.transforms.Resample(sampling_rate, 16000)
-    speech = resampler(speech_array)
-    print(speech)
+    speech = AudioSegment.from_wav(name)
+    json_file_path = './evaluation-1214/result_jsons/' + name.split('\\')[1].split('.')[0] + '_result.json'
+    print(json_file_path)
+    translator = Translator()
 
-    for i in [1]:
-        index_start = math.floor(float(offsets[i]) * 16000)
-        index_end = math.ceil((float(durations[i]) + float(offsets[i])) * 16000)
-        trimmed_speech = speech[index_start:index_end]
+    for i in range(len(offsets)):
+        if i % 50 == 0:
+            translator = Translator()
+        t1 = float(offsets[i]) * 1000
+        t2 = (float(durations[i]) + float(offsets[i])) * 1000
+        trimmed = speech[t1:t2]
 
+        saved_name = './evaluation-1214/audio_train/trimmed/' + name.split('\\')[1].split('.')[0] + '_' + str(i) + '.wav'
+        trimmed.export(saved_name, format="wav")
+    
+        f = codecs.open(json_file_path, 'a', "utf-8")
+        with sr.AudioFile(saved_name) as source:
+            audio = r.record(source)
+            try:
+                s = r.recognize_google(audio, language = 'zh-CN')
+            except Exception as e:
+                s = ''
+            print(s)
+            try:
+                translation = translate_client.translate(s, target_language='en')['translatedText']
+            except Exception as e:
+                translation = ''
+            print(translation)
+
+        json_line = {}
+        json_line["translation"] = translation
+        json_line = json.dumps(json_line)
+        f.write(json_line + '\n')
+        f.close()
         # print('./MT_data/test/' + name.split('\\')[1].split('.')[0] + '_' + str(i) + '.wav')
-        saved_name = './MT_data/test/trimmed/' + name.split('\\')[1].split('.')[0] + '_' + str(i) + '.wav'
-        torchaudio.save('./MT_data/test/trimmed/4.wav', trimmed_speech, sampling_rate)
 
-        # with sr.AudioFile(name) as source:
-        #     audio = r.record(source)
-        #     print(audio)
-        # try:
-        #     s = r.recognize_google(trimmed_speech, language = 'zh-CN')
-        #     translator = Translator()
-        #     translation =  translator.translate(s, src='zh-cn', dest='en').text
-        #     print("Text: "+ s)
-        #     print("Translation:" + translation)
-        # except Exception as e:
-        #     print("Exception: "+str(e))
 
 # def preprocess_function(examples):
 #     inputs = [ex for ex in examples["transcript"]]
